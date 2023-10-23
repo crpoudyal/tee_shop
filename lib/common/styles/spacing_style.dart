72f4ad15459166c1c029eac3fcc69d90efbdd42a
cr poudyal <crpoudyal@crs-MacBook-Air.local>
2023-10-23 16:39:51 +0545

    feat:spacing style

---
+++ lib/common/styles/spacing_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tee_shop/utils/constants/sizes.dart';
+
+class TSpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: TSizes.appBarHeight,
+    left: TSizes.defaultSpace,
+    right: TSizes.defaultSpace,
+    bottom: TSizes.defaultSpace,
+  );
+}
